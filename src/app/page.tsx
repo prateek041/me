ca078938e358407e91452a8af39c953bb7d92836
Prateek Singh <prateeksingh9741@gmail.com>
2024-10-05 19:59:53 +0530

    Add: Home Page My work section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import MainHeader from "./components/MainHeader";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto space-y-20">
       <MainHeader />
+      <About />
     </div>
   );
 }
