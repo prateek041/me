031ce7cbf0a8c82b6f5509e23f8b69271d20a1b7
Prateek Singh <prateeksingh9741@gmail.com>
2024-10-05 19:59:23 +0530

    Add: Home page about section

---
+++ src/app/components/About.tsx
@@ -0,0 +1,23 @@
+import Socials from "./Socials";
+import WhatIDo from "./WhatIDo";
+
+const About = () => {
+  return (
+    <div className="flex flex-col gap-y-16">
+      <div className="grid grid-cols-8">
+        <div className="col-span-5">
+          <h2 className="text-4xl">
+            I’m a builder from India with about 2 years of experience with Web
+            Technologies, Cloud Native, Open Source and Content writing.
+          </h2>
+        </div>
+        <div className="col-start-7 col-end-9">
+          <Socials />
+        </div>
+      </div>
+      <WhatIDo />
+    </div>
+  );
+};
+
+export default About;
