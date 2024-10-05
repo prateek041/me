87b2956aaea9cddc622b0f376c0db22c46d26f7f
Prateek Singh <prateeksingh9741@gmail.com>
2024-10-05 19:59:33 +0530

    Add: home page socials component

---
+++ src/app/components/Socials.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Socials = () => {
+  return (
+    <div>
+      <div>{"Let's get connected"}</div>
+      <div className="flex flex-col">
+        <Link href={"https://www.linkedin.com/in/prateek-singh-903449211/"}>
+          Linkedin
+        </Link>
+        <Link href={"https://x.com/prateek_0041"}>X</Link>
+        <Link href={"https://github.com/prateek041"}>GitHub</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Socials;
