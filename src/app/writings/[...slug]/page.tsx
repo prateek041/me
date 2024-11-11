0761a1961bced4f86aa9365aae7940187da3b395
prateek041 <prateeksingh9741@gmail.com>
2024-11-11 18:46:18 +0530

    adding metadata for each article

--- src/app/writings/[...slug]/page.tsx
+++ src/app/writings/[...slug]/page.tsx
@@ -7,6 +7,13 @@ import ArticleContent from "@/app/components/ArticleContent";
 import Image from "next/image";
 import emoji from "remark-emoji";
 import AudioPlayer from "@/app/components/AudioPlayer";
+import type { Metadata } from "next";
+import Head from "next/head"
+
+export const metadata: Metadata = {
+  title: "writings",
+  description: "A public, written diary of what I write",
+};
 
 const LifeArticle = async ({ params }: { params: { slug: string[] } }) => {
   const pathName = process.cwd() + "/writings";
@@ -25,35 +32,48 @@ const LifeArticle = async ({ params }: { params: { slug: string[] } }) => {
 
   const contentHtml = processedMarkdown.toString();
   return (
-    <div className="container w-full md:mx-10 md:my-5 my-10 scroll-smooth">
-      <div className="flex flex-col items-center md:gap-y-10 gap-y-2">
-        <h1 className="xl:text-8xl lg:text-7xl text-4xl text-center">
-          {matterResult.data.title}
-        </h1>
-        <h3>{matterResult.data.date}</h3>
-        {!isTech && (
-          <div className="">
-            <AudioPlayer audioFile={matterResult.data.audio} />
+    <>
+      <Head>
+        <title>{matterResult.data.title}</title>
+        <meta name="description" content={matterResult.data.description} />
+        <meta property="og:title" content={matterResult.data.title} />
+        <meta property="og:description" content={matterResult.data.description} />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={`https://prateeksingh.tech/writings/${params.slug.join("/")}`} />
+        <meta property="og:image" content={imagePath} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div className="container w-full md:mx-10 md:my-5 my-10 scroll-smooth">
+        <div className="flex flex-col items-center md:gap-y-10 gap-y-2">
+          <h1 className="xl:text-8xl lg:text-7xl text-4xl text-center">
+            {matterResult.data.title}
+          </h1>
+          <h3>{matterResult.data.date}</h3>
+          {!isTech && (
+            <div className="">
+              <AudioPlayer audioFile={matterResult.data.audio} />
+            </div>
+          )}
+        </div>
+        <div className="md:sticky top-10 flex justify-center w-full mt-10">
+          <div className="relative md:-left-8">
+            <Image
+              className="object-cover"
+              src={imagePath}
+              alt="Article Header image"
+              style={{ width: "100vw", height: "100%" }}
+              width={1000}
+              height={1000}
+              quality={100}
+            />
           </div>
-        )}
-      </div>
-      <div className="md:sticky top-10 flex justify-center w-full mt-10">
-        <div className="relative md:-left-8">
-          <Image
-            className="object-cover"
-            src={imagePath}
-            alt="Article Header image"
-            style={{ width: "100vw", height: "100%" }}
-            width={1000}
-            height={1000}
-            quality={100}
-          />
+        </div>
+        <div className="flex w-full max-w-screen-2xl bg-gray-100 relative md:-mt-[calc(80%-300px)] lg:p-[3rem] lg:-mt-[calc(60%-300px)] xl:p-[3rem] xl:-mt-[calc(40%-300px)]">
+          <ArticleContent pageContent={contentHtml} />
         </div>
       </div>
-      <div className="flex w-full max-w-screen-2xl bg-gray-100 relative md:-mt-[calc(80%-300px)] lg:p-[3rem] lg:-mt-[calc(60%-300px)] xl:p-[3rem] xl:-mt-[calc(40%-300px)]">
-        <ArticleContent pageContent={contentHtml} />
-      </div>
-    </div>
+    </>
+
   );
 };
 
